3061b5b4c3fd5a98c7feb35764264d693665dfd9
Junaidsf01 <junaidshareef55@gmail.com>
2026-05-21 15:13:29 +0530

    Revert "feat: integrate Provider for reactive state management and update dashboard with dynamic activity tracking."

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'app.dart';
-import 'features/activities/data/activity_repository.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ActivityRepository()..load(),
-      child: const ChiaCpdApp(),
-    ),
-  );
+  runApp(const ChiaCpdApp());
 }
